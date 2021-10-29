ea579916b4b530ac35118b988a6310cdf73df5a9
Facundo Creus <fcreus@gmail.com>
2021-10-29 15:29:11 -0300

    Cambio carpeta public.

--- src/services/server.ts
+++ src/services/server.ts
@@ -36,7 +36,7 @@ app.use(errorHandler);
 app.use(compression());
 
 // Express & Handlebars Setup
-app.use(express.static(publicFolderPath));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'hbs');
